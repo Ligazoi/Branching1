b5fdb64c70e06173827046dda210d779049fddc4
Ligazoi <liga.zoida@inbox.lv>
2021-10-12 23:14:15 +0300

    Added Switch assignment and main method to Miles

--- src/com/company/Days.java
+++ src/com/company/Days.java
@@ -7,28 +7,28 @@ public class Days {
 
     public static void main (String [] args) {
 
-        Scanner inputDay = new Scanner(System.in);
+        Scanner inputDayFormat = new Scanner(System.in);
         System.out.println("Please write the day of the month");
-        int Day = inputDay.nextInt ();
+        int Day = inputDayFormat.nextInt ();
         if (Day <1 || Day >31) {
             System.out.println("Please enter correct Day of the month");
             System.exit(0);
         }
-        Scanner inputMonth = new Scanner(System.in);
+        Scanner inputMonthFormat = new Scanner(System.in);
         System.out.println("Please choose the month");
-        int Month = inputMonth.nextInt();
+        int Month = inputMonthFormat.nextInt();
         if (Month < 1 || Month > 12) {
             System.out.println("Please enter correct month of the year");
             System.exit(0);
         }
-        Scanner inputYear = new Scanner(System.in);
+        Scanner inputYearFormat = new Scanner(System.in);
         System.out.println("Please choose the year");
-        int Year = inputYear.nextInt();
+        int Year = inputYearFormat.nextInt();
         if (Year <0 || Year > 2500) {
             System.out.println("Please enter correct year");
             System.exit(0);
-
-            Scanner inputDateFormat = new Scanner(System.in);
+        }
+        Scanner inputDateFormat = new Scanner(System.in);
             System.out.println("Please select the date formatting. 1 - YYYY/MM/DD, 2 - YYYY.MM.DD");
              int ChoseDataType = inputDateFormat.nextInt();
             if (ChoseDataType ==1){
@@ -38,12 +38,10 @@ public class Days {
                 System.out.println(Year + "." + Month + "." + Day);
             }
 
-
-
         }
 
 
 
 
     }
-}
+
